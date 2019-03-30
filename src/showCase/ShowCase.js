7de6711d622c295902e41f913fac11088b458709
Emerson Araujo <emersoniago1995@gmail.com>
2019-03-30 15:59:04 -0300

    show case semi pronto

--- src/showCase/ShowCase.js
+++ src/showCase/ShowCase.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './showCase.css';
 import ShowBox from './showcase-box/ShowBox';
 import OwlCarousel from 'react-owl-carousel2';
+import 'react-owl-carousel2/lib/styles.css';
 
 export default class ShowCase extends Component {
 
@@ -17,14 +18,48 @@ export default class ShowCase extends Component {
             onDragged: function (event) { },
             onChanged: function (event) { }
         };
+        const boxes = [
+
+            {
+                url_img: "../../assets/dead.jpg",
+                title: "Site",
+                subtitle: "RH Outdoor"
+
+            }, {
+                url_img: "../../assets/dead.jpg",
+                title: "Aplicativo",
+                subtitle: "Kook"
+            }, {
+                url_img: "../../assets/dead.jpg",
+                title: "Aplicativo",
+                subtitle: "Arte No Fazer"
+            }, {
+                url_img: "../../assets/dead.jpg",
+                title: "Folder",
+                subtitle: "Label design"
+            }, {
+                url_img: "../../assets/dead.jpg",
+                title: "Aplicativo",
+                subtitle: "Kook"
+            }, {
+                url_img: "../../assets/dead.jpg",
+                title: "Aplicativo",
+                subtitle: "Arte No Fazer"
+            }, {
+                url_img: "../../assets/dead.jpg",
+                title: "Aplicativo",
+                subtitle: "Arte No Fazer"
+            }
+
+        ]
         return (
             <div>
+                <h2 className='last-works'>Últimos Trabalhos</h2>
                 <OwlCarousel options={options} events={events} >
-                    <div style={{ background: 'yellow', width: '100px', height: '100px' }}></div>
-                    <div style={{ background: 'yellow', width: '100px', height: '100px' }}></div>
-                    <div style={{ background: 'yellow', width: '100px', height: '100px' }}></div>
-                    <div style={{ background: 'yellow', width: '100px', height: '100px' }}></div>
-                    <div style={{ background: 'yellow', width: '100px', height: '100px' }}></div>
+                    {boxes.map((box) => {
+                        return <ShowBox url_img={box.url_img} title={box.title} subtitle={box.subtitle} ></ShowBox>
+                    })}
+
                 </OwlCarousel>
             </div>
 
--- src/showCase/showcase-box/ShowBox.js
+++ src/showCase/showcase-box/ShowBox.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './showBox.css';
 
+
 export default class ShowBox extends Component {
     render() {
         return (
-            <div className='show-box'>
-
+            <div className='show-box flex-vertical'>
+                <img className='img-box' src={require("../../assets/dead.jpg")}></img>
+                <h3 className='h3-box'>{this.props.title}</h3>
+                <p className='p-box'>{this.props.subtitle}</p>
             </div>
         )
     }
